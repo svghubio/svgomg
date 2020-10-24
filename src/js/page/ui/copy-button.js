e0bfa88ad0653fbd9d277d28aab32adddb678945
svghubio <svghubio@svghubio>
2020-10-24 19:15:03 +0300

    Disable the copying

--- src/js/page/ui/copy-button.js
+++ src/js/page/ui/copy-button.js
@@ -1,6 +1,7 @@
 import FloatingActionButton from './floating-action-button';
 
-export const copySupported = (document.queryCommandSupported && document.queryCommandSupported('copy'));
+//export const copySupported = (document.queryCommandSupported && document.queryCommandSupported('copy'));
+export const copySupported = false;
 
 export default class CopyButton extends FloatingActionButton {
   constructor() {
@@ -11,7 +12,6 @@ export default class CopyButton extends FloatingActionButton {
       iconSvg: (
         '<svg viewBox="0 0 24 24" class="icon">' +
           `<title>${title}</title>` +
-          '<path d="M16 1H4C3 1 2 2 2 3v14h2V3h12V1zm3 4H8C7 5 6 6 6 7v14c0 1 1 2 2 2h11c1 0 2-1 2-2V7c0-1-1-2-2-2zm0 16H8V7h11v14z"/>' +
         '</svg>'
       ),
       minor: true
@@ -23,18 +23,6 @@ export default class CopyButton extends FloatingActionButton {
 
   _onClick(event) {
     super._onClick(event);
-    this._pre.textContent = this._text;
-    document.body.appendChild(this._pre);
-    getSelection().removeAllRanges();
-
-    const range = document.createRange();
-    range.selectNode(this._pre);
-
-    window.getSelection().addRange(range);
-
-    document.execCommand('copy');
-    getSelection().removeAllRanges();
-    document.body.removeChild(this._pre);
   }
 
   setCopyText(text, filename) {
